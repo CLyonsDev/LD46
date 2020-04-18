f41ed36f83b791dd7f5ce2bbed2143854b379272
Collin <collinclyons@gmail.com>
2020-04-17 20:25:26 -0700

    Driveable Car

---
+++ Assets/Scripts/P_CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool Motor;      // Is this wheel attached to the motor?
+    public bool Steering;   // Does this wheel apply to the steer angle?
+}
+
+public class P_CarController : MonoBehaviour
+{
+
+    public List<AxleInfo> axleList; // Information about each axle
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axle in axleList)
+        {
+            if(axle.Steering == true)
+            {
+                axle.leftWheel.steerAngle = steering;
+                axle.rightWheel.steerAngle = steering;
+            }
+            if(axle.Motor == true)
+            {
+                axle.leftWheel.motorTorque = motor;
+                axle.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ResetCar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetCar : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float RaiseDistance = 1.5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.y, 0));
+            rb.MovePosition(Vector3.up * RaiseDistance);
+        }
+    }
+}
