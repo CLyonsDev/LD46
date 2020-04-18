87e38eb62bd423040b2be8462fa1ed5de571d436
Collin <collinclyons@gmail.com>
2020-04-18 16:40:08 -0700

    Added glove, improved hand control

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -10,6 +10,7 @@ public class HandMovement : MonoBehaviour
     public Vector2 HorizontalOffsetClamp;
     public Vector2 VerticalOffsetClamp;
     public Vector2 ForwardBackClamp;
+    public float MinDistanceToCamera;   // How close can we get to the camera before it's too close?
 
     //private Rigidbody rb;
     private bool MoveVertical;
@@ -20,12 +21,14 @@ public class HandMovement : MonoBehaviour
     private Vector2 mousePos;
 
     private ObjectGrabLogic logic;
+    private Camera cam;
 
     // Start is called before the first frame update
     void Awake()
     {
         //rb = GetComponent<Rigidbody>();
         logic = GetComponentInChildren<ObjectGrabLogic>();
+        cam = Camera.main;
     }
 
     private void Start()
@@ -58,10 +61,12 @@ public class HandMovement : MonoBehaviour
         {
             logic.Squeeze();
         }
+
+        HandMoveLogic();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void HandMoveLogic()
     {
         mousePos.x = Input.GetAxis("Mouse X");
         mousePos.y = Input.GetAxis("Mouse Y");
@@ -83,12 +88,17 @@ public class HandMovement : MonoBehaviour
 
         Vector3 finalDest = transform.localPosition + (right + forward + up);
 
-        transform.localPosition = Vector3.Lerp(transform.localPosition, finalDest, HandLerpSpeed * Time.fixedDeltaTime);
-        
-        //TODO: REWRITE
-        transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, HorizontalOffsetClamp.x, HorizontalOffsetClamp.y),
-            Mathf.Clamp(transform.localPosition.y, VerticalOffsetClamp.x, VerticalOffsetClamp.y),
-            Mathf.Clamp(transform.localPosition.z, ForwardBackClamp.x, ForwardBackClamp.y)
-            );
+        //Debug.Log(Vector3.Distance(transform.localPosition + finalDest, cam.transform.localPosition));
+
+        if(Vector3.Distance(transform.localPosition + finalDest, cam.transform.localPosition) >= MinDistanceToCamera)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, finalDest, HandLerpSpeed * Time.deltaTime);
+
+            //TODO: REWRITE
+            transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, HorizontalOffsetClamp.x, HorizontalOffsetClamp.y),
+                Mathf.Clamp(transform.localPosition.y, VerticalOffsetClamp.x, VerticalOffsetClamp.y),
+                Mathf.Clamp(transform.localPosition.z, ForwardBackClamp.x, ForwardBackClamp.y)
+                );
+        }
     }
 }
